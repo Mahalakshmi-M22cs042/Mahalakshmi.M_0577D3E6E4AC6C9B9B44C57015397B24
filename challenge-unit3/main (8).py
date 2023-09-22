eee1f1ba3c6ba97762ccac5ce5ee3a4e1e89817f
Mahalakshmi-M22cs042 <144209635+Mahalakshmi-M22cs042@users.noreply.github.com>
2023-09-22 09:09:53 -0700

    main (8).py

--- challenge-unit3/main (8).py
+++ challenge-unit3/main (8).py
@@ -1,4 +1,6 @@
-#Write a function called linear_search_product that takes the list of products and a target product name as input. The function should perform a linear search to find the target product in the list and return a list of indices of all occurrences of the product if found, or an empty list if the product is not found.
+#Write a function called linear_search_product that takes the list of products and a target product name as input. The function should 
+perform a linear search to find the target product in the list and return a list of indices of all occurrences of the product if found,
+or an empty list if the product is not found.
 
 def linear_search_product(productList, targetProduct):
   indices=[]
